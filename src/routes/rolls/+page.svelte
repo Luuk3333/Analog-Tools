cc62b7a28d625a594c2337bc9f1f4f2ff3d46865
Luuk3333 <Luuk3333@users.noreply.github.com>
2025-02-17 01:58:50 +1030

    Add roll without filling in form

--- src/routes/rolls/+page.svelte
+++ src/routes/rolls/+page.svelte
@@ -5,20 +5,17 @@
 
 	function addRoll(event) {
 		event.preventDefault();
-		const form = event.currentTarget;
-		const formData = new FormData(form);
-
-		// Convert formData to an object
-		let data = Object.fromEntries(formData.entries());
-		data = {
-			...data,
+		const data = {
 			id: self.crypto.randomUUID(),
 			shots: [],
+			brand: "",
+			product: "",
+			iso: null,
+			identifier: "",
 			added_on: new Date().getTime(),
 		};
 		console.log(data);
-		rolls.current = [...rolls.current, data];
-		form.reset();
+		rolls.current = [data, ...rolls.current];
 	}
 </script>
 
@@ -29,63 +26,69 @@
 <h1>Rolls</h1>
 <p>Keep track of your film rolls.</p>
 
-{#if rolls.current.length > 0}
-	<hr />
-	<h2>Your rolls</h2>
-	<ul>
-		{#each rolls.current as roll}
-			<li>
-				{roll.brand}
-				{roll.product}
-				<pre>{JSON.stringify(roll)}</pre>
-				<button
-					onclick={() => {
-						const index = rolls.current.findIndex((r) => r.id === roll.id);
-						console.log(index);
-						if (index !== -1) {
-							rolls.current.splice(index, 1);
-						}
-					}}>Remove</button>
-			</li>
-		{/each}
-	</ul>
-{/if}
-
 <hr />
+<h2>Your rolls</h2>
+<button onclick={addRoll}>Add roll</button>
+<ul>
+	{#each rolls.current as roll}
+		<li>
+			{roll.brand || `Roll #${roll.id.slice(0, 7)}`}
+			{roll.product}
+			<br />
 
-<h2>New roll</h2>
-<form onsubmit={addRoll}>
-	<fieldset>
-		<legend>Roll</legend>
-		<label for="brand">Brand:</label>
-		<input type="text" id="brand" name="brand" list="brands" placeholder="Kodak" /><br /><br />
-		<datalist id="brands">
-			<option value="Kodak"></option>
-			<option value="Ilford"></option>
-		</datalist>
-		<label for="product">Product:</label>
-		<input
-			type="text"
-			id="product"
-			name="product"
-			list="products"
-			placeholder="Portra 400" /><br /><br />
-		<datalist id="products">
-			<option value="Portra 160"></option>
-			<option value="Portra 400"></option>
-			<option value="Portra 800"></option>
-			<option value="Delta 3200"></option>
-		</datalist>
-		<label for="iso">ISO:</label>
-		<input type="text" id="iso" name="iso" placeholder="400" /><br /><br />
-	</fieldset>
-	<fieldset>
-		<legend>Custom data</legend>
-		<label for="identifier">Identifier:</label>
-		<input type="text" id="identifier" name="identifier" placeholder="251A" /><br /><br />
-	</fieldset>
-	<input type="submit" value="Add" />
-</form>
+			{#if roll.editing}
+				<button onclick={() => (roll.editing = false)}>Finish editing</button>
+				<fieldset>
+					<legend>Roll</legend>
+					<label for="brand">Brand:</label>
+					<input
+						type="text"
+						id="brand"
+						bind:value={roll.brand}
+						list="brands"
+						placeholder="Kodak" />
+					<datalist id="brands">
+						<option value="Kodak"></option>
+						<option value="Ilford"></option>
+					</datalist>
+					<br />
+					<label for="product">Product:</label>
+					<input
+						type="text"
+						id="product"
+						bind:value={roll.product}
+						list="products"
+						placeholder="Portra 400" />
+					<datalist id="products">
+						<option value="Portra 160"></option>
+						<option value="Portra 400"></option>
+						<option value="Portra 800"></option>
+						<option value="Delta 3200"></option>
+					</datalist>
+					<br />
+					<label for="iso">ISO:</label>
+					<input type="text" id="iso" bind:value={roll.iso} placeholder="400" />
+				</fieldset>
+				<fieldset>
+					<legend>Custom</legend>
+					<label for="identifier">Identifier:</label>
+					<input type="text" id="identifier" name="identifier" placeholder="251A" />
+				</fieldset>
+				<br />
+			{:else}
+				<button onclick={() => (roll.editing = true)}>Edit</button>
+			{/if}
+			<button
+				onclick={() => {
+					const index = rolls.current.findIndex((r) => r.id === roll.id);
+					console.log(index);
+					if (index !== -1) {
+						rolls.current.splice(index, 1);
+					}
+				}}>Remove</button>
+		</li>
+	{/each}
+</ul>
 
 <hr />
 
