4d173a01a058f72382539a4a58f8d13842b9ea96
Luuk3333 <Luuk3333@users.noreply.github.com>
2025-02-17 13:56:53 +1030

    Put build number in const in service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,9 +2,11 @@ import { build, files, prerendered, version } from "$service-worker";
 import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
 import { buildNumber } from "$lib/buildNumber.js";
 
+const build = buildNumber;
+
 const precache_list = [...build, ...files, ...prerendered].map((s) => ({
 	url: s,
-	revision: `${version}-${buildNumber}`,
+	revision: version,
 }));
 
 precacheAndRoute(precache_list);
